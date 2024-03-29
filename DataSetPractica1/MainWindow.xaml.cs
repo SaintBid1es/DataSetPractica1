1d9cd6747a9917623cd223d61fb55c951cc4edeb
SaintBidies <144172427+SaintBid1es@users.noreply.github.com>
2024-03-29 22:54:21 +0300

    Добавьте файлы проекта.

---
+++ DataSetPractica1/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using DataSetPractica1.MAGAZINEDataSetTableAdapters;
+namespace DataSetPractica1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        
+        ExpensesTableAdapter expenses = new ExpensesTableAdapter();
+        public MainWindow()
+        {
+           
+            InitializeComponent();
+            ExpensesDataGrid.ItemsSource = expenses.GetData();
+        }
+
+        private void button3_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow OneWindow = new MainWindow();
+            OneWindow.Show();
+            Close();
+        }
+
+        private void button3_Click_1(object sender, RoutedEventArgs e)
+        {
+           Window1 window = new Window1();
+            window.Show();
+            Close();
+        }
+    }
+}
---
+++ DataSetPractica1/Window1.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using DataSetPractica1.MAGAZINEDataSetTableAdapters;
+namespace DataSetPractica1
+{
+    /// <summary>
+    /// Логика взаимодействия для Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        CustomersTableAdapter Customers = new CustomersTableAdapter();
+        public Window1()
+        {
+            InitializeComponent();
+            CustomersDataGrid.ItemsSource = Customers.GetData();
+        }
+        private void button3_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow OneWindow = new MainWindow();
+            OneWindow.Show();
+            Close();
+        }
+
+        private void button3_Click_1(object sender, RoutedEventArgs e)
+        {
+            Window1 window = new Window1();
+            window.Show();
+            Close();
+        }
+    }
+}
